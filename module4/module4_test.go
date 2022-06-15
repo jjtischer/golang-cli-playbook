3a09b4943c69cd9fab7247763bb5ed9c6e2968bd
Josh Tischer <josh.tischer@aspenmesh.io>
2022-06-15 11:22:51 -0600

    Completed module

--- module4/module4_test.go
+++ module4/module4_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestModule4GoFormatContent(t *testing.T) {
-	expected := "	github.com/codemodus/kace"
+	expected := "github.com/codemodus/kace"
 	found := OpenFileAndFindNthString("./go.mod.copy", 0, expected)
 	if found != true {
 		t.Errorf("the kace package is not found")
